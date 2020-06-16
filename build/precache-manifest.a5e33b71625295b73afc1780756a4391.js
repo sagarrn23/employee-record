78170057634bea0680b74bc978933b27ae026bbb
Sagar Chaudhari <sagarchaudhari250@gmail.com>
2020-06-16 22:36:52 +0530

    Build

---
+++ build/precache-manifest.a5e33b71625295b73afc1780756a4391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b9c76a9f5765b7e6e5ae3ad81853ef",
+    "url": "/employee-record/index.html"
+  },
+  {
+    "revision": "8142756ab31a8b3d4841",
+    "url": "/employee-record/static/css/main.e0f242e2.chunk.css"
+  },
+  {
+    "revision": "d6ef8196a4c3bcbc4435",
+    "url": "/employee-record/static/js/2.e79a4357.chunk.js"
+  },
+  {
+    "revision": "2c598234c7a24970b5fdf032f602e7bc",
+    "url": "/employee-record/static/js/2.e79a4357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8142756ab31a8b3d4841",
+    "url": "/employee-record/static/js/main.df08ff1a.chunk.js"
+  },
+  {
+    "revision": "1b9eb114253e7027c1e6",
+    "url": "/employee-record/static/js/runtime-main.38aba84f.js"
+  }
+]);
