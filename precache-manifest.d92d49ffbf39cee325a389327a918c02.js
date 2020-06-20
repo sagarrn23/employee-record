694612b62b882312971ec67084e88fcf2275b5df
Sagar Chaudhari <sagarchaudhari250@gmail.com>
2020-06-20 21:02:45 +0530

    Updates

--- precache-manifest.d92d49ffbf39cee325a389327a918c02.js
+++ precache-manifest.d92d49ffbf39cee325a389327a918c02.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e5b9c76a9f5765b7e6e5ae3ad81853ef",
+    "revision": "55b9d57fa518a4bfb3415339496b8543",
     "url": "/employee-record/index.html"
   },
   {
-    "revision": "8142756ab31a8b3d4841",
+    "revision": "495f86fceb9d4df12098",
     "url": "/employee-record/static/css/main.e0f242e2.chunk.css"
   },
   {
-    "revision": "d6ef8196a4c3bcbc4435",
-    "url": "/employee-record/static/js/2.e79a4357.chunk.js"
+    "revision": "7b8c23a9bf3c2c86805e",
+    "url": "/employee-record/static/js/2.155ea7c1.chunk.js"
   },
   {
     "revision": "2c598234c7a24970b5fdf032f602e7bc",
-    "url": "/employee-record/static/js/2.e79a4357.chunk.js.LICENSE.txt"
+    "url": "/employee-record/static/js/2.155ea7c1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8142756ab31a8b3d4841",
-    "url": "/employee-record/static/js/main.df08ff1a.chunk.js"
+    "revision": "495f86fceb9d4df12098",
+    "url": "/employee-record/static/js/main.7474be79.chunk.js"
   },
   {
     "revision": "1b9eb114253e7027c1e6",
